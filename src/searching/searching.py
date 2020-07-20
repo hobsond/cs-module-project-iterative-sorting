059523fbf6e1137cf2a28b26f7b0f42a40baae28
hobsond <60270270+hobsond@users.noreply.github.com>
2020-07-20 15:26:53 -0400

    Update searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,10 @@
 def linear_search(arr, target):
+    
     # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i 
+            
 
 
     return -1   # not found
@@ -9,6 +14,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    low = 0
+    
+    high = len(arr) -1
+    
+    while low <= high:
+        # first get the mid point between t
+        midpoint = (low + high ) //2 
+        
+        if target == arr[midpoint]:
+            return midpoint
+        if target < arr[midpoint]:
+            high = midpoint - 1
+        if target > arr[midpoint]:
+            low = midpoint + 1
     return -1  # not found
